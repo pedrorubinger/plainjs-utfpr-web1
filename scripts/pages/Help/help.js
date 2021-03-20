6b760c67e89d08d827c460982fd6f2b86b61474c
Pedro Rubinger <pedro.rubinger@gmail.com>
2021-03-20 17:47:08 -0300

    feat: adds contact form, fixes css, adds ajax

--- scripts/pages/Help/help.js
+++ scripts/pages/Help/help.js
@@ -1,3 +1,22 @@
+const fetchCEP = async () => {
+  const postalCodeEl = document.getElementById('contactCEP');
+  const stateEl = document.getElementById('contactState');
+
+  if (postalCodeEl && postalCodeEl.value) {
+    const CEP_API_URL = `https://viacep.com.br/ws/${postalCodeEl.value}/json/`;
+    
+    try {
+      const response = await (await fetch(CEP_API_URL)).json();
+
+      if (!response.erro) {
+        stateEl.setAttribute('value', response.uf);
+      } else throw new Error();
+    } catch (error) {
+      stateEl.setAttribute('value', 'Invalid CEP!');
+    }
+  }
+};
+
 const Help = () => {
   const helpContainer = document.getElementById('help-container');
 
